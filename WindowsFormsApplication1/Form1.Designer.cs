7bce9aef5d50e1feb359965a87e079d2aeab3578
dustydog10 <meaderoberts@comcast.net>
2016-04-13 10:49:15 -0700

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace XmlCompareApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openBaseXMLFile = new System.Windows.Forms.Button();
+            this.baseLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openTestXMLFile = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.compareLabel = new System.Windows.Forms.Label();
+            this.compareBtn = new System.Windows.Forms.Button();
+            this.compareTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openBaseXMLFile
+            // 
+            this.openBaseXMLFile.Location = new System.Drawing.Point(108, 39);
+            this.openBaseXMLFile.Name = "openBaseXMLFile";
+            this.openBaseXMLFile.Size = new System.Drawing.Size(115, 27);
+            this.openBaseXMLFile.TabIndex = 0;
+            this.openBaseXMLFile.Text = "Base XML";
+            this.openBaseXMLFile.UseVisualStyleBackColor = true;
+            this.openBaseXMLFile.Click += new System.EventHandler(this.openBaseXMLButton_Click);
+            // 
+            // baseLabel
+            // 
+            this.baseLabel.AutoSize = true;
+            this.baseLabel.Location = new System.Drawing.Point(46, 78);
+            this.baseLabel.MaximumSize = new System.Drawing.Size(239, 0);
+            this.baseLabel.Name = "baseLabel";
+            this.baseLabel.Size = new System.Drawing.Size(0, 13);
+            this.baseLabel.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 113);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(239, 328);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openTestXMLFile
+            // 
+            this.openTestXMLFile.Location = new System.Drawing.Point(383, 39);
+            this.openTestXMLFile.Name = "openTestXMLFile";
+            this.openTestXMLFile.Size = new System.Drawing.Size(115, 27);
+            this.openTestXMLFile.TabIndex = 3;
+            this.openTestXMLFile.Text = "Test XML";
+            this.openTestXMLFile.UseVisualStyleBackColor = true;
+            this.openTestXMLFile.Click += new System.EventHandler(this.openTestXMLFile_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(325, 113);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(239, 328);
+            this.textBox2.TabIndex = 4;
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(322, 78);
+            this.testLabel.MaximumSize = new System.Drawing.Size(239, 0);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(0, 13);
+            this.testLabel.TabIndex = 5;
+            // 
+            // compareLabel
+            // 
+            this.compareLabel.Location = new System.Drawing.Point(612, 78);
+            this.compareLabel.MaximumSize = new System.Drawing.Size(239, 14);
+            this.compareLabel.Name = "compareLabel";
+            this.compareLabel.Size = new System.Drawing.Size(239, 14);
+            this.compareLabel.TabIndex = 8;
+            this.compareLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // compareBtn
+            // 
+            this.compareBtn.Location = new System.Drawing.Point(671, 39);
+            this.compareBtn.Name = "compareBtn";
+            this.compareBtn.Size = new System.Drawing.Size(115, 27);
+            this.compareBtn.TabIndex = 9;
+            this.compareBtn.Text = "Compare XML";
+            this.compareBtn.UseVisualStyleBackColor = true;
+            this.compareBtn.Click += new System.EventHandler(this.compareBtn_Click);
+            // 
+            // compareTextBox
+            // 
+            this.compareTextBox.Location = new System.Drawing.Point(612, 113);
+            this.compareTextBox.Multiline = true;
+            this.compareTextBox.Name = "compareTextBox";
+            this.compareTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.compareTextBox.Size = new System.Drawing.Size(239, 328);
+            this.compareTextBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 470);
+            this.Controls.Add(this.compareTextBox);
+            this.Controls.Add(this.compareBtn);
+            this.Controls.Add(this.compareLabel);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.openTestXMLFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.baseLabel);
+            this.Controls.Add(this.openBaseXMLFile);
+            this.Name = "Form1";
+            this.Text = "XML Analyzer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button openBaseXMLFile;
+        private System.Windows.Forms.Label baseLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openTestXMLFile;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Label compareLabel;
+        private System.Windows.Forms.Button compareBtn;
+        private System.Windows.Forms.TextBox compareTextBox;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlCompareApp
+{
+    public partial class Form1 : Form
+    {
+        List<string> baseAttrList = new List<string>();
+        List<string> testAttrList = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openBaseXMLButton_Click(object sender, EventArgs e)
+        {
+            // dialog init code can be moved into forms designer settings
+            openFileDialog1.Filter = "XML Files (.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+
+            // call the ShowDialog method to show the dialog box.
+            // bool? userClickedOK = openFileDialog1.ShowDialog();
+            openFileDialog1.ShowDialog();
+            // assuming OK clicked
+
+            string XMLFile = openFileDialog1.FileName;
+            // bool userClickedOK = true;
+            baseLabel.Text = XMLFile;
+
+            // parse base XML file
+            parseXMLAttr(XMLFile, baseAttrList);
+
+            // display the list of block_types we have collected
+            textBox1.Text = String.Join(Environment.NewLine, baseAttrList);
+        }
+
+        private void openTestXMLFile_Click(object sender, EventArgs e)
+        {
+            // dialog init code can be moved into forms designer settings
+            openFileDialog1.Filter = "XML Files (.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+
+            // call the ShowDialog method to show the dialog box.
+            openFileDialog1.ShowDialog();
+
+            string XMLFile = openFileDialog1.FileName;
+            // bool userClickedOK = true;
+            testLabel.Text = XMLFile;
+
+            // parse base XML file
+            parseXMLAttr(XMLFile, testAttrList);
+
+            // display the list of block_types we have collected
+            textBox2.Text = String.Join(Environment.NewLine, testAttrList);
+
+        }
+
+        private void parseXMLAttr(string XMLFile, List<string> attrList)
+        {
+            // This is forward-only code. Use XmlDocument to cache whole file in memory.
+            string srchAttrName = "block_type";
+            XmlReader xmlReader = XmlReader.Create(XMLFile);
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    // here's how to see the tag name
+                    // Console.WriteLine(xmlReader.Name);
+
+                    if (xmlReader.HasAttributes)
+                    {
+                        string attrval = xmlReader.GetAttribute(srchAttrName);
+                        if ((attrval != null) && (!attrList.Exists(element => element == attrval)))
+                        {
+                            attrList.Add(attrval);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void compareBtn_Click(object sender, EventArgs e)
+        {
+            int diffCnt = 0;
+
+            foreach (string attr in testAttrList)
+            {
+                if (!baseAttrList.Exists(element => element == attr))
+                {
+                    compareTextBox.Text += attr + Environment.NewLine;
+                    diffCnt++;
+                }
+            }
+
+            compareLabel.Text = diffCnt.ToString() + " new block types found";
+        }
+    }
+}
+
+// original file reader code -- now switch to XMLReader
+// check the response
+//if (userClickedOK == true)
+//{
+// Open the selected file to read.
+//string text = System.IO.File.ReadAllText(XMLFile);
+//textBox1.Text = text;
+//}
+
